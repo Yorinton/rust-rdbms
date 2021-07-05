eb80d46f278003c1e2ce26242910506f7743d8fd
Yorinton <ka2ki.yori@outlook.com>
2021-07-06 01:21:42 +0900

    thread::spawnのクロージャにメインスレッドで定義した値の所有権を奪わせることで、クロージャの生存期間前に参照がなくなることを防ぐ

--- src/main_parallel.rs
+++ src/main_parallel.rs
@@ -18,4 +18,16 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    let v = vec![1, 2, 3];
+    // moveキーワードをつけることで、クロージャにvの所有権を奪わせる
+    // これにより、クロージャの生存期間より先にvがdropすることを防げる
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+    // クロージャで借用したvを、
+    // スレッドの処理実行が終わる前(.join()の前)にdropしている
+    // drop(v);
+
+    handle.join().unwrap();
 }
